ca9f4dbe505e7263fd61fdc411dfc8219a3273fa
oexlye08 <os.alimie@gmail.com>
2023-02-22 15:35:45 +0700

    operator penugasan

---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,27 @@
+/// Operator Penugasan adalah operator yang digunakan untuk menugaskan nilai ke variabel.
+void main() {
+  // Operator penugasan
+  int a = 10;
+  print(a);
+
+  int b = 5;
+  b += 2;
+  print(b);
+
+  int c = 5;
+  c -= 2;
+  print(c);
+
+  int d = 5;
+  d *= 2;
+  print(d);
+
+  int e = 5;
+  e ~/= 2;
+  print(e);
+
+  int f = 5;
+  f %= 2;
+  print(f);
+  
+}
