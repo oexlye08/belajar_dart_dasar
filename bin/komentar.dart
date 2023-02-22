c97621a311bc3239836a6aa5d816f0c02dde7312
oexlye08 <os.alimie@gmail.com>
2023-02-22 09:56:16 +0700

    komentar

---
+++ bin/komentar.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Komentar satu baris
+  print("Hello World!");
+
+  /*
+  Komentar
+  lebih dari
+  satu baris
+  */
+  print("Hello World!");
+
+  /// Komentar dokumentasi
+  print("Hello World!");
+}
