65016f9b4614aeba3c2b30d10cf67dfd189f20a3
oexlye08 <os.alimie@gmail.com>
2023-02-22 15:35:07 +0700

    increment dan decrement

---
+++ bin/increment_decrement.dart
@@ -0,0 +1,28 @@
+/// Increment and decrement operators
+
+void main() {
+  // Increment
+  int a = 10;
+  a++; // a = a + 1
+  print(a);
+  var a2 = 10;
+  var a21 = a2++; 
+  print(a21);
+
+  var a3 = 10;
+  var a31 = ++a3; // a3 = a3 + 1 
+  print(a31);
+
+  // Decrement
+  int b = 10;
+  b--;
+  print(b);
+
+  var b2 = 10;
+  var b21 = b2--;
+  print(b21);
+
+  var b3 = 10;
+  var b31 = --b3;
+  print(b31);
+}
