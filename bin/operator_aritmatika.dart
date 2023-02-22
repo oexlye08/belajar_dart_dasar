695c5b3a72e8b5e0a67fb8431d76861d25c7ee0e
oexlye08 <os.alimie@gmail.com>
2023-02-22 15:35:27 +0700

    operator aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,38 @@
+/// Operator aritmatika adalah operator yang digunakan untuk melakukan operasi matematika
+void main() {
+  // Penjumlahan
+  int a = 10;
+  int b = 5;
+  int c = a + b;
+  print(c);
+
+  // Pengurangan
+  int d = a - b;
+  print(d);
+
+  // Perkalian
+  int e = a * b;
+  print(e);
+
+  // Pembagian dengan hasil (Double Division)
+  double f = a / b;
+  print(f);
+
+  // Pembagian dengan pembulatan ke bawah (Integer Division)
+  int f2 = a ~/ b;
+  print(f2);
+
+  // Modulus
+  int g = a % b;
+  print(g);
+
+  // Increment
+  int h = 10;
+  h++;
+  print(h);
+
+  // Decrement
+  int i = 10;
+  i--;
+  print(i);
+}
