966288dccb34f3b96e5b01f22d6f099c1b946661
oexlye08 <os.alimie@gmail.com>
2023-02-22 15:36:12 +0700

    simbol

---
+++ bin/simbol.dart
@@ -0,0 +1,8 @@
+/// Simbol adalah tipe data yang digunakan untuk menyimpan data yang bersifat unik
+void main() {
+  Symbol symbol = Symbol("Oki Sulton Alimi");
+  var symbol2 = #belajar_dart;
+
+  print(symbol);
+  print(symbol2);
+}
