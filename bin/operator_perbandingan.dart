bdeb85337ae892a0aaf8b2301c93644238164200
oexlye08 <os.alimie@gmail.com>
2023-02-22 15:35:52 +0700

    operator perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,23 @@
+/// Operator perbandingan digunakan untuk membandingkan dua buah nilai
+void main() {
+  // Operator perbandingan
+  int a = 10;
+  int b = 5;
+  bool c = a > b;
+  print(c);
+
+  bool d = a < b;
+  print(d);
+
+  bool e = a >= b;
+  print(e);
+
+  bool f = a <= b;
+  print(f);
+
+  bool g = a == b;
+  print(g);
+
+  bool h = a != b;
+  print(h);
+}
