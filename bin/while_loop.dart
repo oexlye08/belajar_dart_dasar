56c93a47d2253c85c0f3a47ebfc913899786b180
oexlye08 <os.alimie@gmail.com>
2023-02-23 09:33:32 +0700

    while

---
+++ bin/while_loop.dart
@@ -0,0 +1,11 @@
+/// While loop in Dart
+/// While loop is used to execute a block of code for a specific number of times.
+/// 
+
+void main(List<String> args) {
+  var count = 1;
+  while(count <= 10){
+    print("While Count : $count");
+    count++;
+  }
+}
