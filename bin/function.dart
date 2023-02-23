7b6a267378a0f4af8242a4f5d0fa0ca36cf3660f
oexlye08 <os.alimie@gmail.com>
2023-02-23 10:58:28 +0700

    function

---
+++ bin/function.dart
@@ -0,0 +1,10 @@
+/// Function in Dart
+/// Function is a block of code that can be executed by calling it.
+
+void main(List<String> args) {
+ sayHello();
+}
+
+void sayHello() {
+  print("Hello Gaes!");
+}
