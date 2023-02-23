750f8f6addd6fdc1d1b2bec3f3754943c324dd0a
oexlye08 <os.alimie@gmail.com>
2023-02-23 09:33:38 +0700

    do while

---
+++ bin/do_while_loop.dart
@@ -0,0 +1,12 @@
+/// Do while loop in Dart
+/// Do while loop is used to execute a block of code for a specific number of times.
+/// 
+
+void main(List<String> args) {
+  var count = 100;
+
+  do {
+    print("Do While Count : $count");
+    count++;
+  } while (count <= 10);
+}
