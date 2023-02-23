a6d564d878bb0ab396a6f2e74400c763f15beb86
oexlye08 <os.alimie@gmail.com>
2023-02-23 09:33:16 +0700

    for loop

---
+++ bin/for_loop.dart
@@ -0,0 +1,15 @@
+/// For loop in Dart
+/// For loop is used to execute a block of code for a specific number of times.
+/// 
+
+void main(List<String> args) {
+  var count = 1;
+  for(;count <= 10;){
+    print("Count : $count");
+     count++;
+  }
+
+  for(var i = 1; i <= 10; i++){
+    print("Count i : $i");
+  }
+}
