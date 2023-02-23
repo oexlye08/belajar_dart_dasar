17bffe14d61c6ad33f0ca49b375763979469f3e6
oexlye08 <os.alimie@gmail.com>
2023-02-23 10:59:52 +0700

    function short expression

---
+++ bin/function_short_expression.dart
@@ -0,0 +1,9 @@
+
+void main(List<String> args) {
+  var number1 = 10;
+  var number2 = 20;
+  var total = sum(number1, number2);
+  print(total);
+}
+
+int sum(int number1, int number2) => number1 + number2;
