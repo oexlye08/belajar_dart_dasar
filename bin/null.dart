a21bdc4599bc2cd15503649b2501a41fbaadec4a
oexlye08 <os.alimie@gmail.com>
2023-02-23 09:32:43 +0700

    null

---
+++ bin/null.dart
@@ -0,0 +1,12 @@
+/// Null Safety in Dart
+///
+
+void main() {
+  int number1;
+  int? number2;
+  var number3 = null;
+  
+ // print(number1); // Error
+  print(number2); // null
+  print(number3); // null
+}
