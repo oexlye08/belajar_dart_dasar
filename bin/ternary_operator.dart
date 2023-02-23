7626fdcea9706a39a3020ee7a643da8e3d79bb40
oexlye08 <os.alimie@gmail.com>
2023-02-23 09:33:06 +0700

    ternary operatr

---
+++ bin/ternary_operator.dart
@@ -0,0 +1,11 @@
+/// Ternary operator is a conditional operator that is used to evaluate a condition and return a value based on the result of the condition.
+/// It is also known as conditional operator.
+/// It is a shortcut for if-else statement.
+/// It is a one line if-else statement.
+
+void main() {
+  var nilai = 80;
+  var ucapan = nilai >= 80 ? "Selamat anda lulus" : "Maaf anda tidak lulus";
+
+  print(ucapan);
+}
